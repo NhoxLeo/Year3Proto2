290f684f8f7aa1812e47bb0edcdfa13d2ae751a1
iHusker <60858984+iHusker@users.noreply.github.com>
2020-03-02 17:27:14 +1300

    Structure Update

---
+++ Year3Proto2/Assets/Scripts/Structures/Structure.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StructureType
+{
+    WOODCUTTING,
+    MINING
+};
+
+public class Structure : MonoBehaviour
+{
+
+}
---
+++ Year3Proto2/Assets/Scripts/Structures/StructureManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StructureState
+{
+    SELECTING,
+    MOVING
+};
+
+public class StructureManager : MonoBehaviour
+{
+    private List<Structure> structures;
+    private Transform currentStructure;
+    private StructureState structureState = StructureState.SELECTING;
+
+    private void Update()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (structureState == StructureState.MOVING)
+        {
+            if (Physics.Raycast(mouseRay.origin, mouseRay.direction, out hit, Mathf.Infinity, 1 << 10))
+            {
+                if (currentStructure != null)
+                {
+                    float yPosition = currentStructure.position.y;
+
+                    Vector3 gridpos = new Vector3(
+                        Mathf.Floor(hit.point.x),
+                        Mathf.Floor(hit.point.y),
+                        Mathf.Floor(hit.point.z)
+                    );
+
+                    currentStructure.position = new Vector3(
+                        Mathf.Round(gridpos.x),
+                        Mathf.Round(yPosition),
+                        Mathf.Round(gridpos.z)
+                    );
+
+                    if (Input.GetMouseButtonDown(0))structureState = StructureState.SELECTING;
+                }
+            }
+        }
+        else if (structureState == StructureState.SELECTING)
+        {
+            if (Physics.Raycast(mouseRay.origin, mouseRay.direction, out hit, Mathf.Infinity, 1 << 9))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    currentStructure = hit.transform;
+                    structureState = StructureState.MOVING;
+                }
+            }
+        }
+    }
+}
