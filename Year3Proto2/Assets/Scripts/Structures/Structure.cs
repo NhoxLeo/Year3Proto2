83f672d797674a301163933247048241a8729a2d
iHusker <ihuskypuppy@gmail.com>
2020-03-03 13:33:51 +1300

    Added Some More Abstraction

--- Year3Proto2/Assets/Scripts/Structures/Structure.cs
+++ Year3Proto2/Assets/Scripts/Structures/Structure.cs
@@ -1,41 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-
+﻿using UnityEngine;
 public enum StructureType
 {
-    WOODCUTTING,
-    MINING,
-    ARCHER_TOWER,
-    CATAPULT_TOWER
+    RESOURCE,
+    ENVIRONMENT,
+    ATTACK,
+    DEFENSE
 };
 
 public class Structure : MonoBehaviour
 {
-    public StructureType structureType;
-    public float productionTime = 10.0f;
-    private float remainingTime;
+    public Sprite icon;
+    public string displayName;
 
-    public TMP_Text structureName;
-    public TMP_Text remaining;
-    public Slider slider;
+    private StructureType structureType;
 
-    private void Start()
+    public Structure(StructureType structureType)
     {
-        structureName.text = structureType.ToString();
-        remainingTime = productionTime;
-
-        slider.maxValue = productionTime;
+        this.structureType = structureType;
     }
 
-    private void Update()
+    public StructureType GetStructureType()
     {
-        remainingTime -= Time.deltaTime;
-        if (remainingTime <= 0.0f) remainingTime = productionTime;
-
-        slider.value = remainingTime;
-        remaining.text = ((int)remainingTime).ToString() + " Seconds Until Production";
+        return structureType;
     }
 }
--- Year3Proto2/Assets/Scripts/Structures/StructureManager.cs
+++ Year3Proto2/Assets/Scripts/Structures/StructureManager.cs
@@ -11,7 +11,7 @@ public enum StructureState
 public class StructureManager : MonoBehaviour
 {
     private List<Structure> structures;
-    private Transform currentStructure;
+    private Structure currentStructure;
     private StructureState structureState = StructureState.SELECTING;
 
     private void Update()
@@ -24,20 +24,26 @@ public class StructureManager : MonoBehaviour
             if (Physics.Raycast(mouseRay.origin, mouseRay.direction, out hit, Mathf.Infinity, 1 << 10))
             {
                 if (currentStructure != null)
-                { 
+                {
+                    Vector3 newPosition;
+
                     Vector3 gridpos = new Vector3(
                         Mathf.Floor(hit.point.x),
                         Mathf.Floor(hit.point.y),
                         Mathf.Floor(hit.point.z)
                     );
 
-                    currentStructure.position = new Vector3(
+                    newPosition = new Vector3(
                         Mathf.Round(gridpos.x),
-                        Mathf.Round(currentStructure.position.y),
+                        Mathf.Round(currentStructure.transform.position.y),
                         Mathf.Round(gridpos.z)
                     );
 
-                    if (Input.GetMouseButtonDown(0)) structureState = StructureState.SELECTING;
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        currentStructure.transform.position = newPosition;
+                        structureState = StructureState.SELECTING;
+                    }
                 }
             }
         }
@@ -49,7 +55,7 @@ public class StructureManager : MonoBehaviour
                 {
                     if (hit.transform.GetComponent<Structure>() != null)
                     {
-                        currentStructure = hit.transform;
+                        currentStructure = hit.transform.gameObject.GetComponent<Structure>();
                         structureState = StructureState.MOVING;
                     }
                 }
