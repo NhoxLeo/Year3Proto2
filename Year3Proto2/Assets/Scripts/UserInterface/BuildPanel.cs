8f6c759d2ad2fdae8163a42cee857b2388b25bd0
David Morris <pixelgrapher@outlook.co.nz>
2020-03-06 08:08:20 +1300

    Implementing build panel.

---
+++ Year3Proto2/Assets/Scripts/UserInterface/BuildPanel.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BuildPanel : MonoBehaviour
+{
+    public bool showPanel;
+
+    private CanvasGroup canvas;
+    private RectTransform rTrans;
+
+    private GameObject toolTip;
+    
+    public enum ToolTip
+    {
+        None,
+        Archer,
+        Catapult,
+        Farm,
+        Silo,
+        LumberMill,
+        WoodShed,
+        Mine,
+        OreShed
+    }
+    public ToolTip toolTipShown;
+
+    void Start()
+    {
+        canvas = GetComponent<CanvasGroup>();
+        rTrans = GetComponent<RectTransform>();
+        rTrans.DOSizeDelta(new Vector2(64.0f, 212.0f), 0.0f);
+
+        toolTip = transform.Find("BuildPanelTooltip").gameObject;
+        //toolTip.SetActive(true);
+    }
+
+
+    void Update()
+    {
+
+    }
+
+
+    public void TogglePanel()
+    {
+        if (!showPanel)
+        {
+            ShowPanel();
+        }
+        else
+        {
+            HidePanel();
+        }
+    }
+
+    public void ShowPanel()
+    {
+        canvas.DOKill(true);
+        canvas.DOFade(1.0f, 0.2f);
+        canvas.interactable = true;
+        canvas.blocksRaycasts = true;
+        rTrans.DOSizeDelta(new Vector2(1102.0f, 212.0f), 0.4f).SetEase(Ease.OutQuint);
+
+        showPanel = true;
+    }
+
+    public void HidePanel()
+    {
+        canvas.DOKill(true);
+        canvas.DOFade(0.0f, 0.2f);
+        canvas.interactable = false;
+        canvas.blocksRaycasts = false;
+        rTrans.DOSizeDelta(new Vector2(64.0f, 212.0f), 0.4f).SetEase(Ease.OutQuint);
+
+        showPanel = false;
+    }
+
+    public void SetTooltip(int tooltip)
+    {
+
+    }
+}
