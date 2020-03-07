0ae532cd5ddc52a1805c49fb8c708ca4bbda14a9
Samuel Fortune <s.fortune66@gmail.com>
2020-03-07 15:29:20 +1300

    Implement Farm, Granary, Mine, Metal Storage, environments

---
+++ Year3Proto2/Assets/Scripts/Structures/Granary.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Granary : StorageStructure
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StorageStart();
+        resourceType = ResourceType.food;
+        storage = 1000;
+        structureName = "Granary";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPlace()
+    {
+        FindObjectOfType<GameManager>().CalculateStorageMaximum();
+    }
+}
