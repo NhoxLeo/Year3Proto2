72184c21489fdb9005433c4982907355fb85ec76
Samuel Fortune <s.fortune66@gmail.com>
2020-03-07 22:28:16 +1300

    Some Things Not Working

--- Year3Proto2/Assets/Scripts/Structures/Resource/LumberMill.cs
+++ Year3Proto2/Assets/Scripts/Structures/Resource/LumberMill.cs
@@ -4,12 +4,7 @@ using UnityEngine;
 
 public class LumberMill : ResourceStructure
 {
-    private int tileBonus = 0;
     public bool wasPlacedOnForest = false;
-    public float productionTime = 3f;
-    private float remainingTime = 3f;
-    private int batchSize = 5;
-
 
     // Start is called before the first frame update
     void Start()
@@ -23,53 +18,7 @@ public class LumberMill : ResourceStructure
     // Update is called once per frame
     void Update()
     {
-        remainingTime -= Time.deltaTime;
-
-        if (remainingTime <= 0f)
-        {
-            remainingTime = productionTime;
-            FindObjectOfType<GameManager>().AddBatch(new Batch(tileBonus * batchSize, resourceType));
-        }
-    }
-
-    public void CalculateTileBonus()
-    {
-        tileBonus = 1;
-        if (wasPlacedOnForest)
-        {
-            tileBonus++;
-        }
-
-
-        // If the Lumber Mill is placed on a tile...
-        if (attachedTile)
-        {
-            TileBehaviour tileBehaviour = attachedTile.GetComponent<TileBehaviour>();
-
-            // For each possible tile
-            for (int i = 0; i < 4; i++)
-            {
-                if (tileBehaviour.adjacentTiles.ContainsKey((TileBehaviour.TileCode)i))
-                {
-                    TileBehaviour adjTile = tileBehaviour.adjacentTiles[(TileBehaviour.TileCode)i];
-                    GameObject adjStructure = adjTile.GetAttached();
-                    // If there is a structure on the tile...
-                    if (adjStructure)
-                    {
-                        // If that structure is an environment structure...
-                        if (adjStructure.GetComponent<Structure>().GetStructureType() == StructureType.environment)
-                        {
-                            // If the environment structure is a forest...
-                            if (adjStructure.GetComponent<EnvironmentStructure>().GetEnvironmentType() == EnvironmentStructure.EnvironmentType.forest)
-                            {
-                                tileBonus++;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        Debug.Log("New tile bonus for " + gameObject.ToString() + " is " + tileBonus.ToString());
+        ResourceUpdate();
     }
     public void CalculateTileBonus()
     {
@@ -100,9 +49,7 @@ public class LumberMill : ResourceStructure
             }
         }
         Debug.Log("New tile bonus for " + gameObject.ToString() + " is " + tileBonus.ToString());
-
-    public override bool IsStructure(string _structureName)
-    {
-        return _structureName == "Lumber Mill";
     }
+
+
 }
