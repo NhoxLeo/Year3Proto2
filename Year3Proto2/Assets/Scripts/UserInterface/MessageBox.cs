8b31d7cff5a005ceb8ee361a689d0aa51b6214de
David Morris <pixelgrapher@outlook.co.nz>
2020-03-10 08:38:38 +1300

    Starting Messagebox system

---
+++ Year3Proto2/Assets/Scripts/UserInterface/MessageBox.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MessageBox : MonoBehaviour
+{
+    private Tooltip tool;
+    private float timer;
+
+    private TMP_Text displayText;
+
+    void Start()
+    {
+        displayText = GetComponent<TMP_Text>();
+    }
+
+
+    void Update()
+    {
+        if (timer > 0.0f)
+        {
+            timer -= Time.unscaledDeltaTime;
+            tool.showTooltip = true;
+        }
+        else
+        {
+            tool.showTooltip = false;
+        }
+    }
+
+    public void ShowMessage(string message, float time)
+    {
+        displayText.text = message;
+        timer = time;
+
+        if (tool.showTooltip)
+            tool.PulseTip();
+    }
+}
