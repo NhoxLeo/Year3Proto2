cfabff900add25011d12258bc8abe9d0da2e6c7b
David Morris <pixelgrapher@outlook.co.nz>
2020-05-21 15:26:08 +1200

    Research screen ready for linking with manager

--- Year3Proto2/Assets/Scripts/UserInterface/ResearchScreen.cs
+++ Year3Proto2/Assets/Scripts/UserInterface/ResearchScreen.cs
@@ -51,7 +51,8 @@ public class ResearchScreen : MonoBehaviour
 
     private void InitializeCards()
     {
-        //buildings = new Buildings[numberOfBuildings];
+        // Get number of unlockable buildings
+        //buildings = new Buildings[x];
 
         for (int i = 0; i < buildings.Length; i++)
         {
@@ -60,9 +61,10 @@ public class ResearchScreen : MonoBehaviour
             buildings[i].card.transform.SetParent(cardPanel);
             buildings[i].card.transform.localScale = Vector3.one;
 
-            // Instantiate standard upgrade cards
+            // Get number of upgrades per building
+            //buildings[i].upgrades = new Upgrades[x];
 
-            //buildings[i].upgrades = new Upgrades[numberOfUpgrades];
+            // Instantiate standard upgrade cards
             float upgradeLength = Mathf.Clamp(buildings[i].upgrades.Length - 1, 0, 4);
             for (int j = 0; j < upgradeLength; j++)
             {
@@ -76,15 +78,21 @@ public class ResearchScreen : MonoBehaviour
         RefreshCards();
     }
 
+    private void GetResearchInfo()
+    {
+
+    }
+
     private void RefreshCards()
     {
         // Get info from Research Manager
-        // ...here
+        GetResearchInfo();
 
         for (int i = 0; i < buildings.Length; i++)
         {
             // Set info on building cards
             buildings[i].card.transform.Find("BuildingName").GetComponent<TMP_Text>().text = buildings[i].name;
+            Debug.Log(buildings[i].name);
             buildings[i].card.transform.Find("BuildingIcon").GetComponent<Image>().sprite = FindIcon(buildings[i].name);
             buildings[i].card.transform.Find("ResearchInfo/Description").GetComponent<TMP_Text>().text = buildings[i].description;
             buildings[i].card.transform.Find("ResearchInfo/Price").GetComponent<TMP_Text>().text = buildings[i].price.ToString();
@@ -172,9 +180,14 @@ public class ResearchScreen : MonoBehaviour
 
     private Sprite FindIcon(string _name)
     {
-        string formattedName = _name.Replace(" ", "");
-        formattedName = "uiBuilding" + formattedName;
-        Sprite buildingSprite = Resources.Load<Sprite>("BuildingThumbs/" + formattedName);
+        Sprite buildingSprite = null;
+
+        if (_name != null)
+        {
+            string formattedName = _name.Replace(" ", "");
+            formattedName = "uiBuilding" + formattedName;
+            buildingSprite = Resources.Load<Sprite>("BuildingThumbs/" + formattedName);
+        }
 
         if (buildingSprite == null)
         {
