9c66c9e760948201286d85c12209ae4a930edd88
iHusker <60858984+iHusker@users.noreply.github.com>
2020-07-22 18:49:52 +1200

    Intial Features

---
+++ Year3Proto2/Assets/Scripts/UserInterface/Options/OptionCategoryObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionCategoryObject : MonoBehaviour
+{
+    private Button button;
+
+    public void SetButton(Button _button)
+    {
+        button = _button;
+    }
+
+    public Button GetButton()
+    {
+        return button;
+    }
+}
---
+++ Year3Proto2/Assets/Scripts/UserInterface/Options/OptionObject.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using TMPro;
+
+public enum OptionType
+{
+    CheckBox,
+    Slider,
+    Switcher
+}
+
+[SerializeField]
+public class OptionObject : MonoBehaviour
+{
+    [SerializeField] private TMP_Text displayName;
+    [SerializeField] private Transform element;
+    [SerializeField] private OptionElement optionElement;
+    [SerializeField] protected OptionType optionType;
+
+    public OptionObject Initialise(OptionElement _optionElement)
+    {
+        optionElement = _optionElement;
+
+        displayName.text = optionElement.displayName;
+        optionType = optionElement.optionType;
+
+        return this;
+    }
+}
