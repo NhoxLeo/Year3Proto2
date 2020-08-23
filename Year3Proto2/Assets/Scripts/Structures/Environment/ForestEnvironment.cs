c38892c45d64777e0ec157cb9d8f20d5c3f68ee2
Samuel Fortune <s.fortune66@gmail.com>
2020-08-23 21:34:10 +1200

    Adjacent Tiles implemeted for Lumber Mill and Mine

--- Year3Proto2/Assets/Scripts/Structures/Environment/ForestEnvironment.cs
+++ Year3Proto2/Assets/Scripts/Structures/Environment/ForestEnvironment.cs
@@ -4,11 +4,37 @@ using UnityEngine;
 
 public class ForestEnvironment : EnvironmentStructure
 {
+    private bool exploitedState = false;
+    private MeshRenderer meshRenderer;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
         environmentType = EnvironmentType.forest;
         structureName = "Forest Environment";
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (exploited && !exploitedState)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            meshRenderer.enabled = false;
+            exploitedState = true;
+        }
+        if (!exploited && exploitedState)
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+            meshRenderer.enabled = true;
+            exploitedState = false;
+        }
     }
 }
