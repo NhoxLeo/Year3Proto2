7a938cf497fa61a7ce3ceabd17736ba77159d3a7
iHusker <60858984+iHusker@users.noreply.github.com>
2020-08-26 12:36:48 +1200

    Enemy Detection Changes.

--- Year3Proto2/Assets/Scripts/Structures/Defense/DefenseStructure.cs
+++ Year3Proto2/Assets/Scripts/Structures/Defense/DefenseStructure.cs
@@ -23,39 +23,6 @@ public abstract class DefenseStructure : Structure
         villagerWidget.SetTarget(this);*/
     }
 
-    public List<Transform> GetTargetedEnemies()
-    {
-        List<Transform> closestEnemies = new List<Transform>();
-
-        int foundEnemies = 0;
-        float closestDistanceSqr;
-
-        while (foundEnemies < (enemies.Count > enemyTargetAmount ? enemyTargetAmount : enemies.Count))
-        {
-            closestDistanceSqr = Mathf.Infinity;
-
-            foreach (Transform enemy in enemies)
-            {
-                if (!targetedEnemies.Contains(enemy))
-                {
-                    if (enemy.GetComponent<Enemy>().IsBeingObserved())
-                    {
-                        Vector3 directionToTarget = enemy.transform.position - transform.position;
-                        float dSqrToTarget = directionToTarget.sqrMagnitude;
-                        if (dSqrToTarget < closestDistanceSqr)
-                        {
-                            closestDistanceSqr = dSqrToTarget;
-                            closestEnemies.Add(enemy);
-                            foundEnemies += 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        return closestEnemies;
-    }
-
     protected void DetectEnemies()
     {
         SphereCollider rangeCollider = GetComponentInChildren<TowerRange>().GetComponent<SphereCollider>();
