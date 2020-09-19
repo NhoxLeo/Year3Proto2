4f3c433e323438975181310ceb77ace9af7d5e7e
David Morris <pixelgrapher@outlook.co.nz>
2020-09-19 16:01:44 +1200

    Starting lightning bolt effect

---
+++ Year3Proto2/Assets/Scripts/Fx/LightningBolt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningBolt : MonoBehaviour
+{
+    public Transform target;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (target != null)
+        {
+            transform.LookAt(target);
+
+            Vector3 scale = transform.localScale;
+            scale.z = Vector3.Distance(transform.position, target.transform.position);
+            transform.localScale = scale;
+        }
+
+    }
+}
