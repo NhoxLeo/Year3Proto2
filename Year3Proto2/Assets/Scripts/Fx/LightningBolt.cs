c831507b9452ef057ef3e3df366676b1f4927b40
David Morris <pixelgrapher@outlook.co.nz>
2020-09-20 15:38:58 +1200

    Lightning effect functional

--- Year3Proto2/Assets/Scripts/Fx/LightningBolt.cs
+++ Year3Proto2/Assets/Scripts/Fx/LightningBolt.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class LightningBolt : MonoBehaviour
 {
     public Transform target;
+    [SerializeField] private ParticleSystem lightningBolt;
+    [SerializeField] private ParticleSystem sparks;
+
     void Start()
     {
         
@@ -13,8 +16,14 @@ public class LightningBolt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Fire(target);
+        }
+    }
 
-
+    public void Fire(Transform _target)
+    {
         if (target != null)
         {
             transform.LookAt(target);
@@ -22,7 +31,9 @@ public class LightningBolt : MonoBehaviour
             Vector3 scale = transform.localScale;
             scale.z = Vector3.Distance(transform.position, target.transform.position);
             transform.localScale = scale;
-        }
 
+            lightningBolt.Emit(1);
+            sparks.Emit(10);
+        }
     }
 }
